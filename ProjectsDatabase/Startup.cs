5f2f8f7ed14aad3341f42710a9ed0a01e4e405db
bakeri <ian.bakers@ntlworld.com>
2016-06-25 10:47:03 +0100

    Initial Commit

---
+++ ProjectsDatabase/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectsDatabase.Startup))]
+namespace ProjectsDatabase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
